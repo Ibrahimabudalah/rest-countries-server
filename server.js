18d43af05939e8d01740b7e3363b17232697b522
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2023-02-26 05:18:33 -0800

    created a func to read from the data json file, created a route, and used that route to retrive data

--- server.js
+++ server.js
@@ -3,6 +3,13 @@ const app = express();
 const dotenv = require("dotenv")
 
 
+
+//router
+const route = require("./Routes/Countries")
+app.use("/", route)
+
+
+//listner function
 app.listen(process.env.PORT || 8080, () => {
     console.log(`Server is running on port ${process.env.PORT || 8080}`)
 })
