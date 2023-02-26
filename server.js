0b30f999112e7650892d5315de4b310a102693a3
Ibrahim Abdulah <ibrahimibrahim578@yahoo.com>
2023-02-26 06:46:25 -0800

    installed and used the cors package

--- server.js
+++ server.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
 const dotenv = require("dotenv")
+const cors = require("cors")
 
 
+//enabling cors
+
+app.use(cors)
 
 //router
 const route = require("./Routes/Countries")
